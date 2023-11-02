a5463a8bd04c10a492f2d5961f3495b31ce2c4c2
Marek Uroda <uroda.mar@gmail.com>
2023-11-02 21:10:17 +0100

    ASCII Art Update

--- world/world.py
+++ world/world.py
@@ -25,3 +25,23 @@ def gen_world():
                     case 'O':
                         world[i].append(Biom("miasto", 0, 'O'))
     return world
+
+def gen_village_0():
+    village = []
+    for i in range(0, 220):
+        world.append([])
+        for j in range(0, 272):
+            map_char = ascii_art.gotland_map[i*273+j]
+            if not map_char == '\n':
+                match map_char:
+                    case '.':
+                        world[i].append(Biom("morze", 0, '.'))
+                    case '$':
+                        world[i].append(Biom("równina", 1, '$'))
+                    case 'Ñ':
+                        world[i].append(Biom("las", 2, 'Ñ'))
+                    case '⁕':
+                        world[i].append(Biom("plarza", 3, '⁕'))
+                    case 'O':
+                        world[i].append(Biom("miasto", 0, 'O'))
+    return world
